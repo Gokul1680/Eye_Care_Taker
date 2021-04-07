7813444cf9db76ac6db5a28791345d23d4de6992
Gokul <69070641+Gokul1680@users.noreply.github.com>
2021-04-07 22:59:37 +0530

    files added

---
+++ script.js
@@ -0,0 +1,15 @@
+function demo(){
+    interv = 1200000;
+    
+    setInterval(function() {
+        document.getElementById("audio").play();
+        
+        
+    },interv);
+   
+    
+
+}
+function additive_func(){
+    document.getElementById("additive").style.display = 'block';
+}
